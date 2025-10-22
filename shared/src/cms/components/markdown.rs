8cfb89906f302a84588dc2fd5c90885b485b011b
Chris Jackson <hello@cjjackson.dev>
2025-10-22 21:02:02 +0100

    Place component at the end

--- shared/src/cms/components/markdown.rs
+++ shared/src/cms/components/markdown.rs
@@ -3,24 +3,24 @@ use crate::cms::markers::{ComponentDataMarker, ComponentInfoMarker};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct Markdown {
+pub struct MarkdownComponent {
     pub content: String,
     pub parse_html: String,
 }
 
-impl ComponentDataMarker for Markdown {
+impl ComponentDataMarker for MarkdownComponent {
     fn into_data(self) -> Vec<u8> {
         rmp_serde::to_vec(&self).expect("failed to serialize markdown")
     }
 }
 
-impl From<Vec<u8>> for Markdown {
+impl From<Vec<u8>> for MarkdownComponent {
     fn from(data: Vec<u8>) -> Self {
         rmp_serde::from_slice(&data).expect("failed to deserialize markdown")
     }
 }
 
-impl ComponentInfoMarker for Markdown {
+impl ComponentInfoMarker for MarkdownComponent {
     fn component_info() -> CmsComponentInfo {
         CmsComponentInfo {
             kind: "Markdown".to_string(),
