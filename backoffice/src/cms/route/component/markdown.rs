33643b6e96c1fb974fc890078dc79fb774e38114
Chris Jackson <hello@cjjackson.dev>
2025-10-25 16:55:59 +0100

    make the component form all mark ups

--- backoffice/src/cms/route/component/markdown.rs
+++ backoffice/src/cms/route/component/markdown.rs
@@ -1,20 +1,21 @@
 use crate::cms::methods::ComponentMethods;
+use maud::Markup;
 use poem::{get, handler};
 use shared::cms::components::markdown::MarkdownComponent;
 use shared::cms::markers::ComponentInfoMarker;
 
 #[handler]
-fn markdown_component_create() -> poem::Result<poem::Response> {
+fn markdown_component_create() -> poem::Result<Markup> {
     todo!()
 }
 
 #[handler]
-fn markdown_component_fetch() -> poem::Result<poem::Response> {
+fn markdown_component_fetch() -> poem::Result<Markup> {
     todo!()
 }
 
 #[handler]
-fn markdown_component_update() -> poem::Result<poem::Response> {
+fn markdown_component_update() -> poem::Result<Markup> {
     todo!()
 }
 
