61b68ecadf002dbc67bf91d573e7fbdff5d8f7ce
Chris Jackson <hello@cjjackson.dev>
2025-10-31 18:55:18 +0000

    implemented: cms attachment service

--- backoffice/src/cms/service/cms_attachment_service.rs
+++ backoffice/src/cms/service/cms_attachment_service.rs
@@ -1,9 +1,11 @@
-use crate::cms::data_model::cms_data::AddFileAttachmentModel;
+use crate::cms::data_model::cms_data::{AddFileAttachmentModel, ListFileAttachmentModel};
 use crate::cms::repository::cms_repository::CmsRepository;
 use error_stack::{Report, ResultExt};
 use poem::web::Field;
 use shared::utils::config::ConfigPointer;
 use shared::utils::context::{Context, ContextError, FromContext};
+use std::path::Path;
+use std::sync::Arc;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -12,6 +14,8 @@ pub enum CmsAttachmentServiceError {
     DbError,
     #[error("Save error")]
     SaveError,
+    #[error("Not found error")]
+    NotFoundError,
 }
 
 pub struct CmsAttachmentService {
@@ -50,7 +54,13 @@ impl CmsAttachmentService {
             .change_context(CmsAttachmentServiceError::SaveError)
             .attach(poem::http::StatusCode::BAD_REQUEST)?;
 
-        _ = std::fs::create_dir_all(format!("{}/{}", &self.file_upload_path, Self::SAVE_PATH));
+        // save file path dirname
+        let save_file_path = Path::new(&save_file_path);
+
+        _ = std::fs::create_dir_all(save_file_path.parent().ok_or_else(|| {
+            Report::new(CmsAttachmentServiceError::SaveError)
+                .attach(poem::http::StatusCode::BAD_REQUEST)
+        })?);
 
         std::fs::write(save_file_path, file_content)
             .change_context(CmsAttachmentServiceError::SaveError)
@@ -67,6 +77,61 @@ impl CmsAttachmentService {
             .attach(poem::http::StatusCode::INTERNAL_SERVER_ERROR)?;
 
         Ok(())
+    }
+
+    pub fn delete_file_by_component_id(
+        &self,
+        component_id: i64,
+    ) -> Result<(), Report<CmsAttachmentServiceError>> {
+        let list_file = self
+            .cms_repository
+            .list_file_attachment(component_id)
+            .change_context(CmsAttachmentServiceError::DbError)
+            .attach(poem::http::StatusCode::INTERNAL_SERVER_ERROR)?;
+
+        self.cms_repository
+            .delete_file_attachment(component_id)
+            .change_context(CmsAttachmentServiceError::DbError)
+            .attach(poem::http::StatusCode::INTERNAL_SERVER_ERROR)?;
+
+        for file in list_file.iter() {
+            let file_path = format!("{}/{}", &self.file_upload_path, &file.file_path);
+            _ = std::fs::remove_file(file_path);
+        }
+
+        Ok(())
+    }
+
+    pub fn delete_file_by_id(&self, id: i64) -> Result<(), Report<CmsAttachmentServiceError>> {
+        let file = self
+            .cms_repository
+            .get_file_path(id)
+            .change_context(CmsAttachmentServiceError::DbError)
+            .attach(poem::http::StatusCode::INTERNAL_SERVER_ERROR)?
+            .ok_or_else(|| {
+                Report::new(CmsAttachmentServiceError::NotFoundError)
+                    .attach(poem::http::StatusCode::NOT_FOUND)
+            })?;
+
+        self.cms_repository
+            .delete_file_attachment(id)
+            .change_context(CmsAttachmentServiceError::DbError)
+            .attach(poem::http::StatusCode::INTERNAL_SERVER_ERROR)?;
+
+        let file_path = format!("{}/{}", &self.file_upload_path, &file.0);
+        _ = std::fs::remove_file(file_path);
+
+        Ok(())
+    }
+
+    pub fn list_file_by_component_id(
+        &self,
+        component_id: i64,
+    ) -> Result<Arc<[ListFileAttachmentModel]>, Report<CmsAttachmentServiceError>> {
+        self.cms_repository
+            .list_file_attachment(component_id)
+            .change_context(CmsAttachmentServiceError::DbError)
+            .attach(poem::http::StatusCode::INTERNAL_SERVER_ERROR)
     }
 }
 
